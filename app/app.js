b31ce1a064082cb9b4d84014b06d6c8cf7a15677
Lloyd Smith II <1066080+les2@users.noreply.github.com>
2025-01-09 12:49:50 -0500

    configure flowbite for tailwind and ember

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ember-flowbite-example/config/environment';
+import { initFlowbite } from 'flowbite';
+
+initFlowbite();
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
--- config/tailwind/tailwind.config.js
+++ config/tailwind/tailwind.config.js
@@ -8,9 +8,14 @@ const appEntry = path.join(appRoot, 'app');
 const relevantFilesGlob = '**/*.{html,js,ts,hbs,gjs,gts}';
 
 module.exports = {
-  content: [path.join(appEntry, relevantFilesGlob)],
+  content: [
+    path.join(appEntry, relevantFilesGlob),
+    './node_modules/flowbite/**/*.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 };
